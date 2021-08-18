ec15711e2b356af835a9299466b7cfaba392092d
MichaelPisciotta <mfp56@miami.edu>
2021-08-18 15:18:47 -0400

    done

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,14 @@ import React from "react";
 import { name, city } from "../data/data.js";
 
 function Home() {
-  // update the JSX being returned!
-  return <div>Home</div>;
+  return(
+    <div id="home"> 
+    <h1 style={{color:'firebrick', backgroundColor: 'grey'}}>{name} is a Web Developer from {city} </h1>
+    {/* {{color:firebrick;}} --first set of brackets is telling react to eval logic and second is a style object, every object has brackets around */}
+    </div>
+  )
 }
+  
+ 
 
 export default Home;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,13 @@ import React from "react";
 
 function NavBar() {
   // update the JSX being returned!
-  return <nav>NavBar</nav>;
-}
+  return (
+  <nav>
+    <a href="#home">Home</a>
+    <a href="#about">About</a>
+  </nav>
+        )}
 
 export default NavBar;
+
+//to link to ids within a web page, you can use # + id of element i am linking to.
